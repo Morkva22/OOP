97a253bb71b747e8cf6661556884a8e9e1a9a8dc
Mark Maichenko <marcusmay0202@gmail.com>
2025-01-31 20:46:46 +0200

    done..

--- OOP.cpp
+++ OOP.cpp
@@ -1,2 +1,24 @@
+/*/
+Створіть структуру, що описує точку в двовимірній системі координат(x, y).
+За допомогою цієї структури задайте дві точки. Напишіть функцію, яка обчислює відстань між цими двома точками.
+/*/
 #include <iostream>
 using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+int distance(Point p1, Point p2) {
+    int x = p2.x - p1.x;
+    int y = p2.y - p1.y;
+    return sqrt(x * x + y * y);
+}
+
+int main() {
+    Point p1 = {3, 2};
+    Point p2 = {1, 1};
+    cout << "Distance between p1 and p2: " << distance(p1, p2) << endl;
+    return 0;
+}
