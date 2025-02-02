218a6a6bb3c043fdb1d8a6405b3939ecfa1d5e90
Mark Maichenko <marcusmay0202@gmail.com>
2025-02-02 20:48:25 +0200

    +- know how it work!

--- OOP.cpp
+++ OOP.cpp
@@ -2,4 +2,36 @@
 
 /*/
 #include <iostream>
+#include <string>
 using namespace std;
+
+struct Address {
+    string city;
+    string street;
+    int number;
+};
+
+struct Person {
+    string name;
+    int age;
+    Address address;
+};
+
+int main() {
+    Person person;
+    person.name = "Vlad";
+    person.age = 28;
+    
+    person.address.city = "lviv";
+    person.address.street = "magistratska";
+    person.address.number = 22;
+    
+    cout << "name: " << person.name << endl;
+    cout << "yo: " << person.age << endl;
+    cout << "adress: " 
+         << person.address.city << ", " 
+         << person.address.street << " " 
+         << person.address.number << endl;
+
+    return 0;
+}
