02caef08b9a56cdbf65d65c13d63a38ce2949b0d
Mark Maichenko <marcusmay0202@gmail.com>
2025-02-10 20:42:34 +0200

    verif

--- OOP.cpp
+++ OOP.cpp
@@ -4,3 +4,46 @@
  /*/
 #include <iostream>
 using namespace std;
+
+class Transport abstract
+{
+ private:
+    double weight;
+    double speed = 0;
+    double cost;
+    double time;
+    double distance;
+    int passengers;
+    double costPerKm;
+    double timePerKm;
+    double costPerKg;
+    double timePerKg;
+    double costPerWeight;
+    double timePerWeight;
+    
+    public:
+    virtual void CountTime() = 0;
+    virtual void CountCost() = 0;
+    
+};
+
+class Car : public Transport
+{
+private:
+public:
+};
+class Bicycle : public Transport
+{
+private:
+public:
+};
+class Cart : public Transport
+{
+private:
+public:
+};
+
+int main()
+{
+    return 0;
+}
