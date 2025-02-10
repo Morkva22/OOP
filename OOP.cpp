f7c60b74ca68595493bbb6c264e1154241ccb2de
Mark Maichenko <marcusmay0202@gmail.com>
2025-02-10 20:31:34 +0200

    DONE

--- OOP.cpp
+++ OOP.cpp
@@ -6,11 +6,10 @@
 /*/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-
-class Passport
-{
+class Passport {
 private:
     string name;
     string surname;
@@ -24,22 +23,21 @@ private:
     string nubmer_document;
     string Authory;
 
-    
-public: Passport(string name, string surname,string fathername, string date_of_brith,string place_of_birth, string date_of_issue,string date_of_expiry,string nationalitty,string RECO_number,string nubmer_document,string Authory)
-    :name(name),surname(surname),fathername(fathername),date_of_birth(date_of_brith),place_of_birth(place_of_birth),date_of_issue(date_of_issue),
-    date_of_expiry(date_of_expiry),nationalitty(nationalitty),RECO_number(RECO_number),nubmer_document(nubmer_document),Authory(Authory) {}
-    
- void get_name() { cout << name << endl; }
-    void get_surname() { cout << surname << endl; }
-    void get_fathername() { cout << fathername << endl; }
-    void get_date_of_birth() { cout << date_of_birth << endl; }
-    void get_place_of_birth() { cout << place_of_birth << endl; }
-    void get_date_of_issue() { cout << date_of_issue << endl; }
-    void get_date_of_expiry() { cout << date_of_expiry << endl; }
-    void get_nationalitty() { cout << nationalitty << endl; }
-    void get_RECO_number() { cout << RECO_number << endl; }
-    void get_nubmer_document() { cout << nubmer_document << endl; }
-    void get_Authory() { cout << Authory << endl; }
+public:
+    Passport(string name, string surname, string fathername, string date_of_brith, string place_of_birth, string date_of_issue, string date_of_expiry, string nationalitty, string RECO_number, string nubmer_document, string Authory)
+        : name(name), surname(surname), fathername(fathername), date_of_birth(date_of_brith), place_of_birth(place_of_birth), date_of_issue(date_of_issue), date_of_expiry(date_of_expiry), nationalitty(nationalitty), RECO_number(RECO_number), nubmer_document(nubmer_document), Authory(Authory) {}
+
+    string get_name() { return name; }
+    string get_surname() { return surname; }
+    string get_fathername() { return fathername; }
+    string get_date_of_birth() { return date_of_birth; }
+    string get_place_of_birth() { return place_of_birth; }
+    string get_date_of_issue() { return date_of_issue; }
+    string get_date_of_expiry() { return date_of_expiry; }
+    string get_nationalitty() { return nationalitty; }
+    string get_RECO_number() { return RECO_number; }
+    string get_nubmer_document() { return nubmer_document; }
+    string get_Authory() { return Authory; }
 
     void set_name(string name) { this->name = name; }
     void set_surname(string surname) { this->surname = surname; }
@@ -52,14 +50,9 @@ public: Passport(string name, string surname,string fathername, string date_of_b
     void set_RECO_number(string RECO_number) { this->RECO_number = RECO_number; }
     void set_nubmer_document(string nubmer_document) { this->nubmer_document = nubmer_document; }
     void set_Authory(string Authory) { this->Authory = Authory; }
-    
 };
 
-
-
-
-class ForeignPassport : public Passport
-{
+class ForeignPassport : public Passport {
 private:
     string visa;
     string number_passport;
@@ -67,20 +60,19 @@ private:
     string date_of_expiry_passport;
     string place_of_issue_passport;
     string Authory_passport;
+
 public:
     ForeignPassport(string name, string surname, string fathername, string date_of_brith, string place_of_birth, string date_of_issue, string date_of_expiry, string nationalitty, string RECO_number, string nubmer_document, string Authory,
         string visa, string number_passport, string date_of_issue_passport, string date_of_expiry_passport, string place_of_issue_passport, string Authory_passport)
-        :Passport(name, surname, fathername, date_of_brith, place_of_birth, date_of_issue, date_of_expiry, nationalitty, RECO_number, nubmer_document, Authory),
+        : Passport(name, surname, fathername, date_of_brith, place_of_birth, date_of_issue, date_of_expiry, nationalitty, RECO_number, nubmer_document, Authory),
         visa(visa), number_passport(number_passport), date_of_issue_passport(date_of_issue_passport), date_of_expiry_passport(date_of_expiry_passport), place_of_issue_passport(place_of_issue_passport), Authory_passport(Authory_passport) {}
 
-
-    void get_visa() { cout << visa << endl; }
-    void get_number_passport() { cout << number_passport << endl; }
-    void get_date_of_issue_passport() { cout << date_of_issue_passport << endl; }
-    void get_date_of_expiry_passport() { cout << date_of_expiry_passport << endl; }
-    void get_place_of_issue_passport() { cout << place_of_issue_passport << endl; }
-    void get_Authory_passport() { cout << Authory_passport << endl; }
-
+    string get_visa() { return visa; }
+    string get_number_passport() { return number_passport; }
+    string get_date_of_issue_passport() { return date_of_issue_passport; }
+    string get_date_of_expiry_passport() { return date_of_expiry_passport; }
+    string get_place_of_issue_passport() { return place_of_issue_passport; }
+    string get_Authory_passport() { return Authory_passport; }
 
     void set_visa(string visa) { this->visa = visa; }
     void set_number_passport(string number_passport) { this->number_passport = number_passport; }
@@ -90,14 +82,41 @@ public:
     void set_Authory_passport(string Authory_passport) { this->Authory_passport = Authory_passport; }
 };
 
-int main()
-{
-    cout << "--------------------Passport--------------- "<< endl;
+void print_Passport(Passport passport) {
+    cout << "Name: " << passport.get_name() << endl;
+    cout << "Surname: " << passport.get_surname() << endl;
+    cout << "Fathername: " << passport.get_fathername() << endl;
+    cout << "Date of birth: " << passport.get_date_of_birth() << endl;
+    cout << "Place of birth: " << passport.get_place_of_birth() << endl;
+    cout << "Date of issue: " << passport.get_date_of_issue() << endl;
+    cout << "Date of expiry: " << passport.get_date_of_expiry() << endl;
+    cout << "Nationalitty: " << passport.get_nationalitty() << endl;
+    cout << "RECO number: " << passport.get_RECO_number() << endl;
+    cout << "Number document: " << passport.get_nubmer_document() << endl;
+    cout << "Authory: " << passport.get_Authory() << endl;
+}
+
+void print_ForgeinPassport(ForeignPassport Fpassport) {
+    print_Passport(Fpassport);
+    cout << "Visa: " << Fpassport.get_visa() << endl;
+    cout << "Number passport: " << Fpassport.get_number_passport() << endl;
+    cout << "Date of issue passport: " << Fpassport.get_date_of_issue_passport() << endl;
+    cout << "Date of expiry passport: " << Fpassport.get_date_of_expiry_passport() << endl;
+    cout << "Place of issue passport: " << Fpassport.get_place_of_issue_passport() << endl;
+    cout << "Authory passport: " << Fpassport.get_Authory_passport() << endl;
+}
+
+int main() {
+    cout << "--------------------Passport--------------- " << endl;
     Passport p("Ivan", "Ivanov", "Ivanovich", "01.01.1990", "Kiev", "01.01.2010", "01.01.2020", "Ukrainian", "123456", "123456", "Kiev");
-cout << endl<< endl;
+    print_Passport(p);
+
+    cout << endl << endl;
     cout << "--------------------ForeignPassport--------------- " << endl;
     ForeignPassport fp("Ivan", "Ivanov", "Ivanovich", "01.01.1990", "Kiev", "01.01.2010", "01.01.2020", "Ukrainian", "123456", "123456", "Kiev",
         "USA", "123456", "01.01.2010", "01.01.2020", "Kiev", "Kiev");
+    print_ForgeinPassport(fp);
+
     cout << endl << endl;
     return 0;
 }
